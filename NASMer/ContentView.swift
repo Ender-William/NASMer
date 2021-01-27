76b401771956c69514ca915bb193f3908e4ee008
William_Kuang <fuwa165@126.com>
2021-01-27 17:30:07 +0800

    Initial Commit

---
+++ NASMer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NASMer
+//
+//  Created by Willian_Kuang on 2021/1/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NASMer/NASMerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASMerApp.swift
+//  NASMer
+//
+//  Created by Willian_Kuang on 2021/1/27.
+//
+
+import SwiftUI
+
+@main
+struct NASMerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
